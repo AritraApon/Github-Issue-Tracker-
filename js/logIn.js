4395bf5e1ab04f7d70f99ce29fe89c406e84f1ea
AritraApon <aritromazumdar8@gmail.com>
2026-03-07 00:07:54 +0600

    home page header section desing , favicon add

---
+++ js/logIn.js
@@ -0,0 +1,20 @@
+document.getElementById('login-button')
+.addEventListener('click',()=>{
+    const userName = document.getElementById('user-name');
+    const userPassword = document.getElementById('user-password');
+
+    const name = userName.value;
+    const password = userPassword.value;
+    // console.log(name , password)
+    if(name === 'admin' && password === 'admin123'){
+        alert(`Login successful ✅
+        Welcome to GitHub Issue Tracker 🎉
+`)
+ window.location.assign("/home.html")
+    }else{
+        alert(` ⚠️ Login Failed
+    Your Username and Password is incorrect.
+Default userName: admin Default password: admin123`)
+    }
+
+})
