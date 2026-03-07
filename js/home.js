c42b46e236d8be3f085f006440ece7714161edbc
AritraApon <aritromazumdar8@gmail.com>
2026-03-07 22:00:46 +0600

    search btn and footer add

--- js/home.js
+++ js/home.js
@@ -18,7 +18,7 @@ const removeActive = () => {
 }
 
 
-
+//  api load
 async function loadIssue() {
     manageSpinner(true)
     let res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
@@ -28,6 +28,7 @@ async function loadIssue() {
 
 }
 
+// api display
 const displayIssue = (datas) => {
     const issuesContainer = document.getElementById('issues-container');
     issuesContainer.innerHTML = "";
@@ -41,19 +42,19 @@ const displayIssue = (datas) => {
                     <img src="./assets/Open-Status.png" alt="">
                     <p class="px-3 py-1 rounded-2xl border-2 border-[#f6c0c0] font-medium text-[#ef4444] text-center bg-[#feecec] "><span> ${data.priority}  </span></p>
                  </div>
-                 <div>
+                 <div class = "space-y-3">
                     <h1 class="text-xl md:text-2xl font-bold">${data.title} </h1>
                     <p class="font-light line-clamp-2 text-[#6b7a90]">${data.description}</p>
 
                  </div>
                  <div class="flex items-center gap-2">
-                     <p class="px-3 py-1 text-[12px] rounded-2xl border-2 border-[#f6c0c0] font-medium text-[#ef4444] text-center bg-[#feecec] "><i class="fa-solid fa-bug"></i><span>${data.labels[0]} </span></p>
-                     <p class="px-3 py-1 text-[12px] rounded-2xl border-2 border-[#fde68a] font-medium text-[#d97706] text-center bg-[#fff8db] "><i class="fa-solid fa-life-ring"></i><span> ${data.labels[1]}</span></p>
+                     <p class="px-3 py-1 text-[12px] rounded-2xl border-2 border-[#f6c0c0] font-medium text-[#ef4444] text-center bg-[#feecec] "><i class="fa-solid fa-bug"></i><span>${data.labels[0]? data.labels[0] : "No label"} </span></p>
+                     <p class="px-3 py-1 text-[12px] rounded-2xl border-2 border-[#fde68a] font-medium text-[#d97706] text-center bg-[#fff8db] "><i class="fa-solid fa-life-ring"></i><span> ${data.labels[1] ? data.labels[1] : "No label"}</span></p>
                  </div>
                  <div class="divider"></div>
                  <div class="text-[#6b7a90]">
                     <p><span># ${data.id}</span> by <span> ${data.author} </span></p>
-                    <p> ${data.createdAt} </p>
+                    <p> ${data.createdAt.split("T")[0]} </p>
                  </div>
             </div> 
 
@@ -78,7 +79,7 @@ const displayIssue = (datas) => {
 
 }
 
-
+// filter Issues
 async function filterIssues(status) {
     let res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     let data = await res.json();
@@ -94,6 +95,8 @@ async function filterIssues(status) {
     displayIssue(filtered);
 
 }
+
+// all buttons
 const allButton = document.getElementById("all-button");
 const openButton = document.getElementById("open-button");
 const closeButton = document.getElementById("close-button");
@@ -109,7 +112,7 @@ allButton.addEventListener("click", () => {
 openButton.addEventListener("click", () => {
     removeActive()  // remove active class
 
-   openButton.classList.add('active') // add active class
+    openButton.classList.add('active') // add active class
 
 
     filterIssues("open")
@@ -127,3 +130,22 @@ closeButton.addEventListener("click", () => {
 loadIssue()
 
 // loadIssue()
+
+// search button ...
+document.getElementById("btn_search").addEventListener("click", () => {
+    removeActive()
+    const input = document.getElementById("input_search");
+    const searchValue = input.value.trim().toLowerCase();
+
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q= ${searchValue}`)
+        .then(res => res.json())
+        .then(data => {
+               const allIssues = data.data;
+
+            const filterIssues = allIssues.filter((i) =>
+                i.title && i.title.toLowerCase().includes(searchValue)
+            );
+           displayIssue(filterIssues);
+        })
+
+ })
